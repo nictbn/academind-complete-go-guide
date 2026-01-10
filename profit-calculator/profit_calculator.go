ebdb3d4fcf76c0db674a2b0f943952dbfd783273
Nicu Taban <nictbndeveloper@gmail.com>
2026-01-11 01:17:18 +0200

    Functions practice

--- profit-calculator/profit_calculator.go
+++ profit-calculator/profit_calculator.go
@@ -3,25 +3,43 @@ package main
 import "fmt"
 
 func main() {
-	var revenue float64
-	var expenses float64
-	var taxRate float64
+	// var revenue float64
+	// var expenses float64
+	// var taxRate float64
 
-	fmt.Print("Revenue: ")
-	fmt.Scan(&revenue)
+	// fmt.Print("Revenue: ")
+	// fmt.Scan(&revenue)
+	revenue := getUserInput("Revenue: ")
 
-	fmt.Print("Expenses: ")
-	fmt.Scan(&expenses)
+	// fmt.Print("Expenses: ")
+	// fmt.Scan(&expenses)
+	expenses := getUserInput("Expenses: ")
 
-	fmt.Print("Tax Rate: ")
-	fmt.Scan(&taxRate)
+	// fmt.Print("Tax Rate: ")
+	// fmt.Scan(&taxRate)
+	taxRate := getUserInput("Tax Rate: ")
 
+	// earningsBeforeTax := revenue - expenses
+	// profit := earningsBeforeTax * (1 - taxRate / 100)
+	// ratio := earningsBeforeTax / profit
+	earningsBeforeTax, profit, ratio := calculateFinancials(revenue, expenses, taxRate)
+	
+	fmt.Printf("%.2f\n", earningsBeforeTax)
+	fmt.Printf("%.2f\n", profit)
+	fmt.Printf("%.2f\n", ratio)
+}
+
+func getUserInput(text string) float64 {
+	var userInput float64
+	fmt.Print(text)
+	fmt.Scan(&userInput)
+	return userInput
+}
+
+func calculateFinancials(revenue float64, expenses float64, taxRate float64) (float64, float64, float64) {
 	earningsBeforeTax := revenue - expenses
 	profit := earningsBeforeTax * (1 - taxRate / 100)
-
 	ratio := earningsBeforeTax / profit
-	fmt.Println(earningsBeforeTax)
-	fmt.Println(profit)
-	fmt.Println(ratio)
 
+	return earningsBeforeTax, profit, ratio
 }
