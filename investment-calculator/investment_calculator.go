1b32225f6fd141723042d5b73e49b765e4d19df7
Nicu Taban <nictbndeveloper@gmail.com>
2026-01-10 22:46:34 +0200

    Example of using constants

--- investment-calculator/investment_calculator.go
+++ investment-calculator/investment_calculator.go
@@ -6,10 +6,13 @@ import (
 )
 
 func main() {
+	const inflationRate = 2.5
 	var investmentAmount float64 = 1000
 	expectedReturnRate := 5.5
 	var years float64 = 10
 
 	futureValue := investmentAmount * math.Pow((1+expectedReturnRate/100), years)
+	futureRealValue := futureValue / math.Pow(1+inflationRate/100, years)
 	fmt.Println(futureValue)
+	fmt.Println(futureRealValue)
 }
