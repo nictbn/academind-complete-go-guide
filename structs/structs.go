ec313fc7fe9a0c7c2f2acf4831abc98536c4c6ce
Nicu Taban <nictbndeveloper@gmail.com>
2026-01-13 20:29:07 +0200

    Creating and instantiating a struct

--- structs/structs.go
+++ structs/structs.go
@@ -2,12 +2,28 @@ package main
 
 import (
 	"fmt"
+	"time"
 )
+
+type user struct {
+	firstName string
+	lastName string
+	birthDate string
+	createdAt time.Time
+}
 
 func main() {
 	firstName := getUserData("Please enter your first name: ")
 	lastName := getUserData("Please enter your last name: ")
 	birthdate := getUserData("Please enter your birthdate (MM/DD/YYYY): ")
+	
+	var appUSer user
+	appUSer = user{
+		firstName: firstName,
+		lastName: lastName,
+		birthDate: birthDate,
+		createdAt: time.Now(),
+	}
 
 	// ... do something awesome with that gathered data!
 
