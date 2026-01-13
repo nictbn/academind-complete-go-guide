6fa030686936c072aae98be5223fde7eb2478bbc
Nicu Taban <nictbndeveloper@gmail.com>
2026-01-13 17:57:06 +0200

    Packages initial commit

---
+++ working-with-packages/bank.go
@@ -0,0 +1,86 @@
+package main
+
+import "errors"
+import "fmt"
+import "os"
+import "strconv"
+
+const accountBalanceFile = "balance.txt"
+
+func main() {
+	var accountBalance, err = getBalanceFromFile()
+	if err != nil {
+		fmt.Println("ERROR")
+		fmt.Println(err)
+		fmt.Println("__________")
+		panic("Can't continue, sorry!")
+	}
+	fmt.Println("Welcome to Go Bank!")
+	for {
+		fmt.Println("What do you want to do?")
+		fmt.Println("1. Check balance")
+		fmt.Println("2. Deposit money")
+		fmt.Println("3. Withdraw money")
+		fmt.Println("4. Exit")
+
+		var choice int
+		fmt.Print("Your choice: ")
+		fmt.Scan(&choice)
+		fmt.Println("You chose: ", choice)
+
+		switch choice {
+		case 1:
+			fmt.Println("Your balance is ", accountBalance)
+		case 2:
+			var depositAmount float64
+			fmt.Print("Your deposit: ")
+			fmt.Scan(&depositAmount)
+			accountBalance += depositAmount
+			if depositAmount <= 0 {
+				fmt.Println("Inva;od amount. Must be greater than 0.")
+				continue
+			}
+			fmt.Println("Balance updated! New amount: ", accountBalance)
+			writeBalanceToFile(accountBalance)
+		case 3:
+			var withdrawalAmount float64
+			fmt.Print("Withdrawal amount: ")
+			fmt.Scan(&withdrawalAmount)
+			if withdrawalAmount <= 0 {
+				fmt.Println("Inva;od amount. Must be greater than 0.")
+				continue
+			}
+			if withdrawalAmount > accountBalance {
+				fmt.Println("Inva;od amount. You can't withdraw more than you have.")
+				continue
+			}
+			accountBalance -= withdrawalAmount
+			fmt.Println("Balance updated! New amount: ", accountBalance)
+			writeBalanceToFile(accountBalance)
+		default:
+			fmt.Println("Goodbye")
+			fmt.Println("Thanks for choosing our bank")
+			return
+			// break breaks out of the switch statement, not out of the loop
+		}
+	}
+	fmt.Println("Thanks for choosing our bank")
+}
+
+func getBalanceFromFile() (float64, error) {
+	data, err := os.ReadFile(accountBalanceFile)
+	if err != nil {
+		return 1000, errors.New("Failed to find balance file.")
+	}
+	balanceText := string(data)
+	balance, err := strconv.ParseFloat(balanceText, 64)
+	if err != nil {
+		return 1000, errors.New("Failed to parse stored balance value.")
+	}
+	return balance, nil
+}
+
+func writeBalanceToFile(balance float64) {
+	balanceText := fmt.Sprint(balance)
+	os.WriteFile(accountBalanceFile, []byte(balanceText), 0644)
+}
