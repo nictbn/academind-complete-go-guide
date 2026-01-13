5245c763d9d7b7910dd850e3fa447c427ca7e6c5
Nicu Taban <nictbndeveloper@gmail.com>
2026-01-13 19:02:27 +0200

    Pass pointer to function

--- pointers/pointers.go
+++ pointers/pointers.go
@@ -4,11 +4,13 @@ import "fmt"
 
 func main() {
 	age := 32
+	// var agePointer *int
+	agePointer := &age
 	fmt.Println("Age: ", age)
-	adultYears := getAdultYears(age)
+	adultYears := getAdultYears(agePointer)
 	fmt.Println(adultYears)
 }
 
-func getAdultYears(age int) int {
-	return age - 18
+func getAdultYears(age *int) int {
+	return *age - 18
 }
