cc72958829708504675db8e0823af6afb57acd05
Nicu Taban <nictbndeveloper@gmail.com>
2026-01-13 21:25:00 +0200

    Adding methods to structs

--- structs/structs.go
+++ structs/structs.go
@@ -12,6 +12,10 @@ type user struct {
 	createdAt time.Time
 }
 
+func (u user) outputUserDetails() {
+	fmt.Println(u.firstName, u.lastName, u.birthDate)
+}
+
 func main() {
 	firstName := getUserData("Please enter your first name: ")
 	lastName := getUserData("Please enter your last name: ")
@@ -39,11 +43,7 @@ func main() {
 	// ... do something awesome with that gathered data!
 
 	// fmt.Println(firstName, lastName, birthdate)
-	outputUserDetails(&appUser)
-}
-
-func outputUserDetails(u *user) {
-	fmt.Println(u.firstName, u.lastName, u.birthDate)
+	appUser.outputUserDetails()
 }
 
 func getUserData(promptText string) string {
