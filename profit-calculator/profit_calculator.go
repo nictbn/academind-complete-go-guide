e4465da4abeef600027ca051dce576db2ecd778e
Nicu Taban <nictbndeveloper@gmail.com>
2026-01-13 17:45:43 +0200

    Exercise solution

--- profit-calculator/profit_calculator.go
+++ profit-calculator/profit_calculator.go
@@ -1,6 +1,8 @@
 package main
 
 import "fmt"
+import "errors"
+import "os"
 
 func main() {
 	// var revenue float64
@@ -9,16 +11,29 @@ func main() {
 
 	// fmt.Print("Revenue: ")
 	// fmt.Scan(&revenue)
-	revenue := getUserInput("Revenue: ")
+	revenue, err := getUserInput("Revenue: ")
+
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
 
 	// fmt.Print("Expenses: ")
 	// fmt.Scan(&expenses)
-	expenses := getUserInput("Expenses: ")
+	expenses, err := getUserInput("Expenses: ")
 
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
 	// fmt.Print("Tax Rate: ")
 	// fmt.Scan(&taxRate)
-	taxRate := getUserInput("Tax Rate: ")
+	taxRate, err := getUserInput("Tax Rate: ")
 
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
 	// earningsBeforeTax := revenue - expenses
 	// profit := earningsBeforeTax * (1 - taxRate / 100)
 	// ratio := earningsBeforeTax / profit
@@ -27,13 +42,17 @@ func main() {
 	fmt.Printf("%.2f\n", earningsBeforeTax)
 	fmt.Printf("%.2f\n", profit)
 	fmt.Printf("%.2f\n", ratio)
+	storeResults(earningsBeforeTax, profit, ratio)
 }
 
-func getUserInput(text string) float64 {
+func getUserInput(text string) (float64, error) {
 	var userInput float64
 	fmt.Print(text)
 	fmt.Scan(&userInput)
-	return userInput
+	if userInput <= 0 {
+		return 0, errors.New("Value must be positive")
+	}
+	return userInput, nil
 }
 
 func calculateFinancials(revenue float64, expenses float64, taxRate float64) (float64, float64, float64) {
@@ -42,4 +61,9 @@ func calculateFinancials(revenue float64, expenses float64, taxRate float64) (fl
 	ratio := earningsBeforeTax / profit
 
 	return earningsBeforeTax, profit, ratio
+}
+
+func storeResults(ebt, profit, ratio float64) {
+	results := fmt.Sprintf("EBT: %.1f\nProfit: %.1f\nRatio: %.3f\n", ebt, profit, ratio)
+	os.WriteFile("results.txt", []byte(results), 0644)
 }
