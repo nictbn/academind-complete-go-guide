34ce1bd04b413864751b7e2dcf66047bdbf1452d
Nicu Taban <nictbndeveloper@gmail.com>
2026-01-14 17:23:26 +0200

    Array example

---
+++ managing-related-data/go.mod
@@ -0,0 +1,3 @@
+module example.com/managing-related-data
+
+go 1.25.5
---
+++ managing-related-data/lists.go
@@ -0,0 +1,8 @@
+package main
+
+import "fmt"
+
+func main() {
+	prices := [4]float64{10.99, 9.99, 45.99, 20.0}
+	fmt.Println(prices)
+}
