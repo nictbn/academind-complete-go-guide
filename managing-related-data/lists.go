b8d1fee5fc5bca9eac94fa28a04b5f6921aefffb
Nicu Taban <nictbndeveloper@gmail.com>
2026-01-14 17:28:23 +0200

    Working with arrays

--- managing-related-data/lists.go
+++ managing-related-data/lists.go
@@ -3,8 +3,12 @@ package main
 import "fmt"
 
 func main() {
-	var productNames [4]string
+	var productNames [4]string = [4]string{"A book"}
 	prices := [4]float64{10.99, 9.99, 45.99, 20.0}
 	fmt.Println(prices)
+
+	productNames[2] = "A Carpet"
 	fmt.Println(productNames)
+
+	fmt.Println(prices[2])
 }
