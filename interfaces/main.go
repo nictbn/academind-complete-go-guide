44d00dd70a84751ba3a6ff2bb96bcb414df21b4f
Nicu Taban <nictbndeveloper@gmail.com>
2026-01-14 16:01:15 +0200

    Interface preparations

--- interfaces/main.go
+++ interfaces/main.go
@@ -5,11 +5,29 @@ import (
 	"fmt"
 	"os"
 	"strings"
+
 	"example.com/note/note"
+	"example.com/note/todo"
 )
 
 func main() {
 	title, content := getNoteData()
+	todoText := getUserInput("Todo text: ")
+	todo, err := todo.New(todoText)
+
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	todo.Display()
+	err = todo.Save()
+	if err != nil {
+		fmt.Println("Saving the todo failed!")
+		return
+	}
+	fmt.Println("Saving the todo succeeded!")
+
 	userNote, err := note.New(title, content)
 
 	if err != nil {
