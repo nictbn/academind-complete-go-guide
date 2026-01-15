3cf22908bf220314ebfeff71164aa8bbe8273f8b
Nicu Taban <nictbndeveloper@gmail.com>
2026-01-15 23:12:23 +0200

    Make maps

--- managing-related-data/main.go
+++ managing-related-data/main.go
@@ -9,4 +9,12 @@ func main() {
 	userNames = append(userNames, "Max")
 	userNames = append(userNames, "Manuel")
 	fmt.Println(userNames)
+
+	courseRatings := make(map[string]float64, 3)
+
+	courseRatings["go"] = 4.7
+	courseRatings["react"] = 4.8
+	courseRatings["angular"] = 4.7
+	courseRatings["node"] = 4.8
+	fmt.Println(courseRatings)
 }
