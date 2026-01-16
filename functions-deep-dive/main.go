9cda46eea4345b99af7b0167507a9fd6de031b77
Nicu Taban <nictbndeveloper@gmail.com>
2026-01-16 10:56:18 +0200

    Returning functions

--- functions-deep-dive/main.go
+++ functions-deep-dive/main.go
@@ -6,10 +6,19 @@ type transformFn func(int) int
 
 func main() {
 	numbers := []int{1, 2, 3, 4}
+	moreNumbers := []int{5, 1, 2}
 	doubled := transformNumbers(&numbers, double)
 	fmt.Println(doubled)
 	tripled := transformNumbers(&numbers, triple)
 	fmt.Println(tripled)
+	transformFn1 := getTransformerFunction(&numbers)
+	transformFn2 := getTransformerFunction(&moreNumbers)
+
+	transformedNumbers := transformNumbers(&numbers, transformFn1)
+	moreTransformedNumbers := transformNumbers(&moreNumbers, transformFn2)
+	fmt.Println(transformedNumbers)
+	fmt.Println(moreTransformedNumbers)
+
 }
 
 func transformNumbers(numbers *[]int, transform transformFn) []int {
@@ -18,6 +27,14 @@ func transformNumbers(numbers *[]int, transform transformFn) []int {
 		dNumbers = append(dNumbers, transform(val))
 	}
 	return dNumbers
+}
+
+func getTransformerFunction(numbers *[]int) transformFn {
+	if (*numbers)[0] == 1 {
+		return double
+	} else {
+		return triple
+	}
 }
 
 func double(number int) int {
