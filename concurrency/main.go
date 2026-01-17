d8f876e892191bed9f17e1c3c4b63d67715c453f
Nicu Taban <nictbndeveloper@gmail.com>
2026-01-17 15:35:49 +0200

    Single channel elegant solution

--- concurrency/main.go
+++ concurrency/main.go
@@ -17,17 +17,25 @@ func slowGreet(phrase string, doneChan chan bool) {
 }
 
 func main() {
-	dones := make([]chan bool, 4)
-	dones[0] = make(chan bool)
-	go greet("Nice to meet you!", dones[0])
-	dones[1] = make(chan bool)
-	go greet("How are you?", dones[1])
-	dones[2] = make(chan bool)
-	go slowGreet("How ... are ... you ...?", dones[2])
-	dones[3] = make(chan bool)
-	go greet("I hope you're liking the course!", dones[3])
+	done := make(chan bool)
+	// dones := make([]chan bool, 4)
+	// dones[0] = make(chan bool)
+	go greet("Nice to meet you!", done)
+	// dones[1] = make(chan bool)
+	go greet("How are you?", done)
+	// dones[2] = make(chan bool)
+	go slowGreet("How ... are ... you ...?", done)
+	// dones[3] = make(chan bool)
+	go greet("I hope you're liking the course!", done)
 
-	for _, done := range dones {
-		<-done
+	// for _, done := range dones {
+	// 	<-done
+	// }
+	count := 0
+	for range done {
+		count++
+		if count == 4 {
+			break
+		}
 	}
 }
