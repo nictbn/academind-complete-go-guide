162246c4e08c42260fd865719fabe4aa4393a775
Nicu Taban <nictbndeveloper@gmail.com>
2026-01-17 16:40:22 +0200

    Use the select statement to await for either done channels or error channels

--- concurrency-project/main.go
+++ concurrency-project/main.go
@@ -10,12 +10,14 @@ import (
 func main() {
 	taxRates := []float64{0, 0.07, 0.1, 0.15}
 	doneChans := make([]chan bool, len(taxRates))
+	errorChans := make([]chan error, len(taxRates))
 	for index, taxRate := range taxRates {
 		doneChans[index] = make(chan bool)
+		errorChans[index] = make(chan error)
 		fm := filemanager.New("prices.txt", fmt.Sprintf("result_%.0f.json", taxRate*100))
 		// cmdm := cmdmanager.New()
 		priceJob := prices.NewTaxIncludedPriceJob(fm, taxRate)
-		go priceJob.Process(doneChans[index])
+		go priceJob.Process(doneChans[index], errorChans[index])
 
 		// if err != nil {
 		// 	fmt.Println("Could not process job")
@@ -23,7 +25,14 @@ func main() {
 		// }
 	}
 
-	for _, donedoneChan := range doneChans {
-		<-donedoneChan
+	for index := range taxRates {
+		select {
+		case err := <-errorChans[index]:
+			if err != nil {
+				fmt.Println(err)
+			}
+		case <-doneChans[index]:
+			fmt.Println("Done")
+		}
 	}
 }
