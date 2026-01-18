e84047f76d86d04b8ad4b16684b012da7b16f23f
Nicu Taban <nictbndeveloper@gmail.com>
2026-01-18 16:37:11 +0200

    Add JWT tokens

--- rest-api/routes/users.go
+++ rest-api/routes/users.go
@@ -4,6 +4,7 @@ import (
 	"net/http"
 
 	"example.com/rest-api/models"
+	"example.com/rest-api/utils"
 	"github.com/gin-gonic/gin"
 )
 
@@ -35,7 +36,10 @@ func login(context *gin.Context) {
 		context.JSON(http.StatusUnauthorized, gin.H{"message": "Could not authenticate user."})
 		return
 	}
-
-	context.JSON(http.StatusOK, gin.H{"message": "Login successful!"})
-
+	token, err := utils.GenerateToken(user.Email, user.ID)
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{"message": "Could not authenticate user"})
+		return
+	}
+	context.JSON(http.StatusOK, gin.H{"message": "Login successful!", "token": token})
 }
