706d530a345f8e8b326523e879984fb9db96a0ce
Nicu Taban <nictbndeveloper@gmail.com>
2026-01-18 17:37:13 +0200

    Bugfixxes

--- rest-api/models/event.go
+++ rest-api/models/event.go
@@ -17,7 +17,7 @@ type Event struct {
 
 var events []Event = []Event{}
 
-func (e Event) Save() error {
+func (e *Event) Save() error {
 	query := `
 	INSERT INTO events(name, description, location, dateTime, user_id)
 	VALUES (?, ?, ?, ?, ?)`
