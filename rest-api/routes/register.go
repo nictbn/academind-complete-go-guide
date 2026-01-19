4f9499987c93d2c3cbf621ebd47e53b286696fff
Nicu Taban <nictbndeveloper@gmail.com>
2026-01-19 18:50:58 +0200

    Add cancel registration feature

--- rest-api/routes/register.go
+++ rest-api/routes/register.go
@@ -21,12 +21,25 @@ func registerForEvent(context *gin.Context) {
 	}
 	err = event.Register(userId)
 	if err != nil {
-		context.JSON(http.StatusInternalServerError, gin.H{"message": "Could not register message for event."})
+		context.JSON(http.StatusInternalServerError, gin.H{"message": "Could not register user for event."})
 		return
 	}
 	context.JSON(http.StatusCreated, gin.H{"message": "Registered!"})
 }
 
 func cancelRegistration(context *gin.Context) {
-
+	userId := context.GetInt64("userId")
+	eventid, err := strconv.ParseInt(context.Param("id"), 10, 64)
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"message": "Could not parse event id."})
+		return
+	}
+	var event models.Event
+	event.ID = eventid
+	err = event.CancelRegistration(userId)
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{"message": "Could not cancel registration."})
+		return
+	}
+	context.JSON(http.StatusOK, gin.H{"message": "Cancelled!"})
 }
